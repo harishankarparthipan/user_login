4aafe3634df175d62ec08a110cc77e9f5acd620e
harishankarparthipan <harishankarparthipan@gmail.com>
2020-11-06 15:38:23 +0530

    commit

--- user_login_form/views.py
+++ user_login_form/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from .forms import CreateUserForm
 from django.contrib import messages
